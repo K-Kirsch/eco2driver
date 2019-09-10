e9ad4950179401b7f4168ebe111bddd1c6f19c9a
Karabonga <S.Schroeer97@web.de>
2019-09-10 17:38:05 +0200

    sdgsdgsdgds

---
+++ eco2driver/Assets/Scripts/GoalBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalBehaviour : MonoBehaviour
+{
+    public ParticleSystem ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    
+    void OnTriggerEnter(Collider c)
+    {
+        Instantiate(ps, transform.position, Quaternion.identity);
+    }
+}
